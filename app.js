82bcbb1959b8ffb1b5c484f501ae50987d37d9a8
Ben Eren <ben@Bens-MacBook-Air.local>
2025-01-21 09:17:56 +0000

    Crud API

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
-const routerFruits = require("./routes/fruits.js")
+const router = require("./routes/fruits.js")
+const logger = require("./logger.js")
+
+app.use('/',logger)
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
-app.use('/fruits', routerFruits)
+app.use('/fruits', router)
 
 module.exports = app
